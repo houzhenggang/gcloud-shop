c6d0d02c8c899551357f3da45cca73eb34656511
janenone2163.com <janenone@163.com>
2016-06-11 15:03:23 +0800

    add:package-info.java

---
+++ gcloud-shop-domain/src/main/java/com/gcloud/shop/domain/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * @Title: package-info
+ * @Package com.gcloud.shop.domain
+ * @Description: ${TODO}(用一句话描述该文件做什么)
+ * @author ChenJin
+ * @date 2016/6/11 14:59
+ * @version V1.0
+ */
+package com.gcloud.shop.domain;
---
+++ gcloud-shop-dubbo/src/main/java/com/gcloud/shop/dubbo/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * @Title: package-info
+ * @Package com.gcloud.shop.dubbo
+ * @Description: ${TODO}(用一句话描述该文件做什么)
+ * @author ChenJin
+ * @date 2016/6/11 15:00
+ * @version V1.0
+ */
+package com.gcloud.shop.dubbo;
