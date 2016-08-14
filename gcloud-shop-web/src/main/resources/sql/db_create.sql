def1237d2556e2125fa58f9b3e915137f4e592d9
janenone@163.com <janenone@163.com>
2016-08-14 16:27:29 +0800

    add:db_init_area.sql

--- gcloud-shop-web/src/main/resources/sql/db_create.sql
+++ gcloud-shop-web/src/main/resources/sql/db_create.sql
@@ -1,13 +1,39 @@
-  CREATE DATABASE IF NOT EXISTS gcloud_demo;
-  USE gcloud_demo;
+  CREATE DATABASE IF NOT EXISTS gcloud_shop;
+  USE gcloud_shop;
   SET FOREIGN_KEY_CHECKS = 0;
-  DROP TABLE IF EXISTS `ts_area`;
-  CREATE TABLE `ts_area` (
-    `id`        BIGINT(20) PRIMARY KEY,
-    `type`      TINYINT(4)   DEFAULT NULL,
-    `name`      VARCHAR(128) DEFAULT '',
-    `parent_id` BIGINT(20)   DEFAULT NULL,
-    `zip`       VARCHAR(128) DEFAULT ''
-  ) ENGINE = InnoDB  AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8;
 
-  图片保存到门店表
+  -- ----------------------------
+  -- Table structure for tb_shop_area
+  -- 地区信息
+  -- ----------------------------
+  DROP TABLE IF EXISTS `tb_shop_area`;
+  CREATE TABLE `tb_area` (
+    `id` bigint(20) NOT NULL,
+    `type` tinyint(4) DEFAULT NULL,
+    `name` varchar(128) DEFAULT '',
+    `parent_id` bigint(20) DEFAULT NULL,
+    `zip` varchar(128) DEFAULT '',
+    PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+  -- ----------------------------
+  -- Table structure for tb_shop_warehouse
+  -- 仓库信息
+  -- ----------------------------
+  DROP TABLE IF EXISTS `tb_shop_warehouse`;
+  CREATE TABLE `tb_shop_warehouse` (
+    `id`             BIGINT(30)   PRIMARY KEY AUTO_INCREMENT,
+    `company_id`     BIGINT(30)   DEFAULT NULL COMMENT '公司ID',
+    `store_id`       BIGINT(30)   DEFAULT NULL COMMENT '门店ID',
+    `warehouse_id`   BIGINT(30)   DEFAULT NULL COMMENT '仓库ID',
+    `name`           VARCHAR(255) DEFAULT "默认仓储" COMMENT '默认仓储',
+    `address`        VARCHAR(255) DEFAULT '' COMMENT '仓库地址',
+    `location`       VARCHAR(255) DEFAULT '' COMMENT '高德地址（经纬度）',
+    `created`       TIMESTAMP   NOT NULL DEFAULT '2000-01-01 00:00:00',
+    `modified`      TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `enable_status` TINYINT(1)  NOT NULL DEFAULT 1,
+    KEY `inx_company_id` (`company_id`),
+    KEY `inx_store_id` (`store_id`),
+    KEY `inx_company_id_store` (`company_id`,`store_id`)
+  ) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = UTF8;
+
