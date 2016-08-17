a8a11b674ef0c4e147e782deb5654364d7845243
janenone2163.com <janenone@163.com>
2016-08-17 12:34:25 +0800

    add:gcloud-shop-cache

---
+++ gcloud-shop-mq/src/main/java/com/gcloud/shop/mq/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * @Title: package-info
+ * @Package com.gcloud.shop.sync
+ * @Description: ${TODO}(用一句话描述该文件做什么)
+ * @author ChenJin
+ * @date 2016/6/11 15:00
+ * @version V1.0
+ */
+package com.gcloud.shop.mq;
