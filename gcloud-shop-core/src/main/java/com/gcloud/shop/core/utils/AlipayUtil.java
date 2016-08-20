df1bdf11f6fdc557941708846dc37db59e84b8e7
janenone@163.com <janenone@163.com>
2016-08-20 09:58:27 +0800

    add:alipay-sdk

---
+++ gcloud-shop-core/src/main/java/com/gcloud/shop/core/utils/AlipayUtil.java
@@ -0,0 +1,12 @@
+package com.gcloud.shop.core.utils;
+
+/**
+ * @author chenjin
+ * @version V1.0
+ * @Title: AlipayUtil
+ * @Package com.gcloud.shop.core.utils
+ * @Description: ${TODO}(用一句话描述该文件做什么)
+ * @date 16-8-20 上午9:28
+ */
+public class AlipayUtil {
+}
